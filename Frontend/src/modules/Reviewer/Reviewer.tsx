58552641503dac9403bc51f434bbabfba7fa17db
techyNonso <williamikeji@gmail.com>
2023-06-17 09:36:47 +0100

    reviwer page done but not reviwer reviwed list

--- Frontend/src/modules/Reviewer/Reviewer.tsx
+++ Frontend/src/modules/Reviewer/Reviewer.tsx
@@ -1,13 +1,43 @@
+import { useEffect, useState } from "react";
 import Header from "@modules/Shared/layout/Header";
 import { ReviewerSummary } from "./features/reviewerSummary";
 import { ReviewList } from "./features/reviewList";
+import { useParams } from "react-router-dom";
+import { getReviewerInfo } from "@modules/Shared/Services";
+interface IData {
+  name: string;
+  description: string;
+  verification_method: string;
+  email: string;
+  location: string;
+  expertise: string[];
+}
 
 export function Reviewer() {
+  let { id } = useParams();
+  const [data, setData] = useState<IData>({
+    name: "string",
+    description: "string",
+    verification_method: "string",
+    email: "string",
+    location: "string",
+    expertise: [""],
+  });
+
+  const getInfo = async () => {
+    const info = await getReviewerInfo(Number(id));
+    setData(info);
+  };
+
+  useEffect(() => {
+    getInfo();
+  }, []);
+
   return (
     <div className="h-full w-full flex flex-col  ">
       <Header />
       <main className="w-full h-[90%]">
-        <ReviewerSummary />
+        <ReviewerSummary data={data} />
         <ReviewList />
       </main>
     </div>
--- Frontend/src/modules/Reviewer/features/reviewerSummary/ReviewerSummary.tsx
+++ Frontend/src/modules/Reviewer/features/reviewerSummary/ReviewerSummary.tsx
@@ -1,59 +1,49 @@
-export const ReviewerSummary = () => {
+interface IData {
+  name: string;
+  description: string;
+  verification_method: string;
+  email: string;
+  location: string;
+  expertise: string[];
+}
+
+export const ReviewerSummary = ({ data }: { data: IData }) => {
   return (
     <div className="h-[60%] w-full relative bg-white">
       <h2 className="font-bold text-5xl h-[15%] bg-white relative pt-3">
-        {" "}
-        Kyoto University
+        {data.name}
       </h2>
       <div className="h-[85%]   relative overflow-y-auto pb-4">
-        <p className="text-xl py-6 ">
-          Kyoto University is a public research university located in Kyoto,
-          Japan. Founded in 1897, it is one of the former Imperial Universities
-          and the second oldest university in Japan. KyotoU is consistently
-          ranked amongst the top two in Japan, the top ten in Asia, and the
-          world's top fifty institutions of higher education.
-        </p>
+        <p className="text-xl py-6 ">{data.description}</p>
 
         <div className="w-full  flex gap-6">
           <div className="flex py-2">
             <div className=" font-bold text-lg">Location: </div>
-            <div className="text-lg pl-3"> Kyoto, Japan</div>
+            <div className="text-lg pl-3"> {data.location}</div>
           </div>
         </div>
         <div className="w-full  flex gap-6">
           <div className="flex py-2">
             <div className=" font-bold text-lg">Email: </div>
-            <div className="text-lg pl-3"> contact@kyoto-u.ac.jp Reviewer</div>
+            <div className="text-lg pl-3"> {data.email}</div>
           </div>
         </div>
         <div className="w-full  flex gap-6">
           <div className="flex py-2">
-            <div className=" font-bold text-lg">
-              Reviewer Verified Through:{" "}
-            </div>
-            <div className="text-lg pl-3"> Email</div>
+            <div className=" font-bold text-lg">Reviewer Verified Through:</div>
+            <div className="text-lg pl-3"> {data.verification_method}</div>
           </div>
         </div>
 
         <div className="flex py-2">
           <div className=" font-bold text-lg">Expertise: </div>
         </div>
-        <div className="flex py-2 text-lg pl-2 ">
-          <div className="">1.</div>
-          <div className=" pl-3">Pharmaceutical Sciences </div>
-        </div>
-        <div className="flex py-2 text-lg pl-2 ">
-          <div className="">2.</div>
-          <div className=" pl-3">Agriculture </div>
-        </div>
-        <div className="flex py-2 text-lg pl-2 ">
-          <div className="">3.</div>
-          <div className=" pl-3">Economics </div>
-        </div>
-        <div className="flex py-2 text-lg pl-2 ">
-          <div className="">4.</div>
-          <div className=" pl-3">Bio Studies </div>
-        </div>
+        {data.expertise.map((item, idx) => (
+          <div className="flex py-2 text-lg pl-2 " key={idx}>
+            <div className="">{idx + 1}.</div>
+            <div className=" pl-3">{item} </div>
+          </div>
+        ))}
       </div>
     </div>
   );

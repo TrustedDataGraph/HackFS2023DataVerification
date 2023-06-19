8e1b8fc1c3b7865e9c5ce55249b4c8fe2d539d84
techyNonso <williamikeji@gmail.com>
2023-06-19 07:59:24 +0100

    server error

--- Frontend/src/modules/Shared/Services/Storage.ts
+++ Frontend/src/modules/Shared/Services/Storage.ts
@@ -1,20 +1,20 @@
-import { Web3Storage } from 'web3.storage'
+//@ts-ignore
+import { Web3Storage } from "web3.storage";
 
 const URL_POST = ".ipfs.w3s.link/";
 const token = `${import.meta.env.VITE_WEB3STORAGE_TOKEN}`;
 
-function makeStorageClient () {
-  return new Web3Storage({ token })
+function makeStorageClient() {
+  return new Web3Storage({ token });
 }
 
-export async function storeFiles (files:any) {
-  try{
-    const client = makeStorageClient()
-    const cid = await client.put(files)
-    console.log('stored files with cid:', cid)
+export async function storeFiles(files: any) {
+  try {
+    const client = makeStorageClient();
+    const cid = await client.put(files);
+    console.log("stored files with cid:", cid);
     return "https://" + cid + URL_POST + files[0].name;
-  } catch(error){
+  } catch (error) {
     console.error(error);
   }
 }
-
